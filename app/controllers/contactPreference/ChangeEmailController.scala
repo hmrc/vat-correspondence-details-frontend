24b433344ce5ffcf252ad35ec7938ede123cca64
dnyuar <60218795+dnyuar@users.noreply.github.com>
2020-07-20 15:42:55 +0100

    [BTAT-8061] - ChangeEmailController progress

--- app/controllers/contactPreference/ChangeEmailController.scala
+++ app/controllers/contactPreference/ChangeEmailController.scala
@@ -16,23 +16,92 @@
 
 package controllers.contactPreference
 
-import config.AppConfig
+import audit.AuditingService
+import audit.models.AttemptedEmailAddressAuditModel
+import common.SessionKeys
+import config.{AppConfig, ErrorHandler}
 import controllers.BaseController
 import controllers.predicates.AuthPredicateComponents
 import controllers.predicates.inflight.InFlightPredicateComponents
-import javax.inject.Inject
-import play.api.mvc.{Action, AnyContent}
+import forms.EmailForm._
+import javax.inject.{Inject, Singleton}
+import play.api.mvc._
+import services.VatSubscriptionService
+import views.html.email.CaptureEmailView
 
-class ChangeEmailController @Inject()(implicit val appConfig: AppConfig,
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class ChangeEmailController @Inject()(val vatSubscriptionService: VatSubscriptionService,
+                                      val errorHandler: ErrorHandler,
+                                      val auditService: AuditingService,
+                                      captureEmailView: CaptureEmailView)
+                                     (implicit val appConfig: AppConfig,
+                                      mcc: MessagesControllerComponents,
                                       authComps: AuthPredicateComponents,
-                                      inFlightPredicateComponents: InFlightPredicateComponents) extends BaseController {
+                                      inFlightComps: InFlightPredicateComponents) extends BaseController {
 
-  //TODO: Update as part of BTAT-8061
-  def show: Action[AnyContent] = (contactPreferencePredicate andThen paperPrefPredicate) { implicit user =>
-    if(appConfig.features.letterToConfirmedEmailEnabled()) {
-      Ok
-    } else {
-      NotFound(authComps.errorHandler.notFoundTemplate)
+  implicit val ec: ExecutionContext = mcc.executionContext
+
+  def show: Action[AnyContent] = (contactPreferencePredicate andThen inFlightEmailPredicate).async { implicit user =>
+
+    val validationEmail: Future[Option[String]] = user.session.get(SessionKeys.validationEmailKey) match {
+      case Some(email) => Future.successful(Some(email))
+      case _ => vatSubscriptionService.getCustomerInfo(user.vrn) map {
+
+        case Right(details) => Some(details.ppob.contactDetails.flatMap(_.emailAddress).getOrElse(""))
+        case _ => None
+      }
+    }
+
+    val prepopulationEmail: Future[String] = validationEmail map { validation =>
+      user.session.get(SessionKeys.prepopulationEmailKey)
+        .getOrElse(validation.getOrElse(""))
+    }
+
+    for {
+      validation    <- validationEmail
+      prepopulation <- prepopulationEmail
+
+    } yield {
+      validation match {
+        case Some(valEmail) =>
+          Ok(captureEmailView(emailForm(valEmail).fill(prepopulation), emailNotChangedError = false, valEmail))
+            .addingToSession(SessionKeys.validationEmailKey -> valEmail)
+        case _ => NotFound(authComps.errorHandler.notFoundTemplate)
+      }
     }
   }
+
+
+  def submit: Action[AnyContent] = (contactPreferencePredicate andThen inFlightEmailPredicate).async { implicit user =>
+    val validationEmail: Option[String] = user.session.get(SessionKeys.validationEmailKey)
+    val prepopulationEmail: Option[String] = user.session.get(SessionKeys.prepopulationEmailKey)
+
+    (validationEmail, prepopulationEmail) match {
+      case (Some(validation), _) => emailForm(validation).bindFromRequest.fold(
+        errorForm => {
+          val notChanged: Boolean = errorForm.errors.head.message == user.messages.apply("captureEmail.error.notChanged")
+          Future.successful(BadRequest(captureEmailView(errorForm, notChanged, validation)))
+
+        },
+        email     => {
+          auditService.extendedAudit(
+            AttemptedEmailAddressAuditModel(
+              Option(validation).filter(_.nonEmpty),
+              email,
+              user.vrn,
+              user.isAgent,
+              user.arn
+            ),
+            controllers.email.routes.CaptureEmailController.submit().url
+          )
+          Future.successful(Redirect("")
+            .addingToSession(SessionKeys.prepopulationEmailKey -> email)) // TODO - Redirect to confirmation page
+        }
+      )
+      case (None, _) => Future.successful(errorHandler.showInternalServerError)
+    }
+  }
+
 }
